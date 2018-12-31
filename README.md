62e85b0ba18c5af109c7ccf9e338305a7401bfb0
Scott Muc <scottmuc@gmail.com>
2018-12-31 09:54:15 +0100

    Added scottmuc.com

--- README.md
+++ README.md
@@ -10,5 +10,8 @@
   @jezhumble for this sharing how the [US Government manages DNS][18f-dns]
 * `s3` backend for Terraform state because I repave frequently so need to ensure these files are
   stored somewhere safe.
+* Separate `terraform` user created with: `IAMFullAccess`, `AmazonS3FullAccess`, and `AmazonRoute53FullAccess`
+  policies. Created manually as I wasn't sure how to `terraform` the user that `terraform` runs as. Never used
+  the **Access Advisor** before and it's pretty slick!
 
 [18f-dns]: https://18f.gsa.gov/2018/08/15/shared-infrastructure-as-code/
---
+++ dns/scottmuc.com.tf
@@ -0,0 +1,61 @@
+
+resource "aws_route53_zone" "scottmuc_com" {
+  name = "scottmuc.com."
+}
+
+
+resource "aws_route53_record" "scottmuc_com" {
+  zone_id = "${aws_route53_zone.scottmuc_com.zone_id}"
+  name = "${aws_route53_zone.scottmuc_com.name}"
+  type = "A"
+  ttl = "3600"
+  records = [
+    "192.30.252.153",
+    "192.30.252.154"
+  ]
+}
+
+
+resource "aws_route53_record" "mx-scottmuc_com" {
+  zone_id = "${aws_route53_zone.scottmuc_com.zone_id}"
+  name = "${aws_route53_zone.scottmuc_com.name}"
+  type = "MX"
+  ttl = "3600"
+  records = [
+    "90 aspmx2.google.com.",
+    "90 aspmx3.google.com.",
+    "50 alt1.aspmx.l.google.com.",
+    "10 aspmx.l.google.com.",
+    "50 alt2.aspmx.l.google.com.",
+    "90 aspmx4.google.com.",
+    "90 aspmx5.google.com."
+  ]
+}
+
+
+resource "aws_route53_record" "www_scottmuc_com" {
+  zone_id = "${aws_route53_zone.scottmuc_com.zone_id}"
+  name = "www.${aws_route53_zone.scottmuc_com.name}"
+  type = "CNAME"
+  ttl = "3600"
+  records = ["scottmuc.com."]
+}
+
+
+resource "aws_route53_record" "feeds_scottmuc_com" {
+  zone_id = "${aws_route53_zone.scottmuc_com.zone_id}"
+  name = "feeds.${aws_route53_zone.scottmuc_com.name}"
+  type = "CNAME"
+  ttl = "3600"
+  records = ["18y3nfm.feedproxy.ghs.google.com."]
+}
+
+
+resource "aws_route53_record" "mail_scottmuc_com" {
+  zone_id = "${aws_route53_zone.scottmuc_com.zone_id}"
+  name = "mail.${aws_route53_zone.scottmuc_com.name}"
+  type = "CNAME"
+  ttl = "3600"
+  records = ["ghs.google.com."]
+}
+
