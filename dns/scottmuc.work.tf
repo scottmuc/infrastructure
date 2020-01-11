c1d1cdb4b483ed0783ceed8f52551f710387b192
Scott Muc <scottmuc@gmail.com>
2020-01-11 20:17:52 +0100

    figured out what I was missing with gandi resources

--- dns/scottmuc.work.tf
+++ dns/scottmuc.work.tf
@@ -2,6 +2,17 @@ data "gandi_zone" "scottmuc_work" {
   name = "scottmuc.work"
 }
 
+resource "gandi_zonerecord" "at_scottmuc_work" {
+  zone = data.gandi_zone.scottmuc_work.id
+  name = "@"
+  type = "A"
+  ttl = 3600
+  values = [
+    "192.168.0.1"
+  ]
+}
+
+
 resource "gandi_zonerecord" "foo_scottmuc_work" {
   zone = data.gandi_zone.scottmuc_work.id
   name = "foo"
@@ -12,3 +23,18 @@ resource "gandi_zonerecord" "foo_scottmuc_work" {
   ]
 }
 
+resource "gandi_zonerecord" "mx_scottmuc_work" {
+  zone = data.gandi_zone.scottmuc_work.id
+  name = "@"
+  type = "MX"
+  ttl = 3600
+  values = [
+    "10 foo.mx.mail",
+    "50 foo.mx.mail",
+  ]
+}
+
+resource "gandi_domainattachment" "scotttmuc_work" {
+  zone   = data.gandi_zone.scottmuc_work.id
+  domain = "scottmuc.work"
+}
