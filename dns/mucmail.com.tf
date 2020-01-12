8cc0f4f9208ec6c73dfba5a345f3af15736f2fdb
Scott Muc <scottmuc@gmail.com>
2020-01-12 14:18:03 +0100

    Switch mucmail.com back to gandi DNS

--- dns/mucmail.com.tf
+++ dns/mucmail.com.tf
@@ -4,6 +4,11 @@ resource "aws_route53_zone" "mucmail_com" {
 }
 
 
+data "gandi_zone" "mucmail_com" {
+  name = "mucmail.com"
+}
+
+
 resource "aws_route53_record" "mx-mucmail_com" {
   zone_id = aws_route53_zone.mucmail_com.zone_id
   name = aws_route53_zone.mucmail_com.name
@@ -18,4 +23,26 @@ resource "aws_route53_record" "mx-mucmail_com" {
     "5 ASPMX4.GOOGLEMAIL.COM",
     "5 ASPMX5.GOOGLEMAIL.COM."
   ]
+}
+
+resource "gandi_zonerecord" "mx_at_mucmail_com" {
+  zone = data.gandi_zone.mucmail_com.id
+  name = "@"
+  type = "MX"
+  ttl = "3600"
+  values = [
+    "1 ASPMX.L.GOOGLE.COM.",
+    "3 ALT1.ASPMX.L.GOOGLE.COM.",
+    "3 ALT2.ASPMX.L.GOOGLE.COM.",
+    "5 ASPMX2.GOOGLEMAIL.COM.",
+    "5 ASPMX3.GOOGLEMAIL.COM.",
+    "5 ASPMX4.GOOGLEMAIL.COM",
+    "5 ASPMX5.GOOGLEMAIL.COM."
+  ]
+}
+
+
+resource "gandi_domainattachment" "mucmail_com" {
+  zone   = data.gandi_zone.mucmail_com.id
+  domain = "mucmail.com"
 }
