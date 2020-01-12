bd777c581e2cbd13a3a98c031943be36d24ae93c
Scott Muc <scottmuc@gmail.com>
2020-01-12 14:12:45 +0100

    Switched devopsbookmarks back to gandi DNS

--- dns/devopsbookmarks.com.tf
+++ dns/devopsbookmarks.com.tf
@@ -3,6 +3,12 @@ resource "aws_route53_zone" "devopsbookmarks_com" {
   name = "devopsbookmarks.com."
 }
 
+
+data "gandi_zone" "devopsbookmarks_com" {
+  name = "devopsbookmarks.com"
+}
+
+
 resource "aws_route53_record" "devopsbookmarks_com" {
   zone_id = aws_route53_zone.devopsbookmarks_com.zone_id
   name = aws_route53_zone.devopsbookmarks_com.name
@@ -11,10 +17,39 @@ resource "aws_route53_record" "devopsbookmarks_com" {
   records = ["217.70.184.38"]
 }
 
+
+resource "gandi_zonerecord" "at_devopsbookmarks_com" {
+  zone = data.gandi_zone.devopsbookmarks_com.id
+  name = "@"
+  type = "A"
+  ttl = "3600"
+  values = [
+    "217.70.184.38"
+  ]
+}
+
+
 resource "aws_route53_record" "www_devopsbookmarks_com" {
   zone_id = aws_route53_zone.devopsbookmarks_com.zone_id
   name = "www.${aws_route53_zone.devopsbookmarks_com.name}"
   type = "CNAME"
   ttl = "10800"
   records = ["devops-bookmarks.herokuapp.com"]
+}
+
+
+resource "gandi_zonerecord" "www_devopsbookmarks_com" {
+  zone = data.gandi_zone.devopsbookmarks_com.id
+  name = "www"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "devops-bookmarks.herokuapp.com"
+  ]
+}
+
+
+resource "gandi_domainattachment" "devopsbookmarks_com" {
+  zone   = data.gandi_zone.devopsbookmarks_com.id
+  domain = "devopsbookmarks.com"
 }
