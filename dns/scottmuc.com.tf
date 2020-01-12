57abf7b7799a34fb2b2fc65cd7b093460b5af58b
Scott Muc <scottmuc@gmail.com>
2020-01-12 13:58:16 +0100

    Switching to gandi.net for all DNS hosting

--- dns/scottmuc.com.tf
+++ dns/scottmuc.com.tf
@@ -4,12 +4,31 @@ resource "aws_route53_zone" "scottmuc_com" {
 }
 
 
+data "gandi_zone" "scottmuc_com" {
+  name = "scottmuc.com"
+}
+
+
 resource "aws_route53_record" "scottmuc_com" {
   zone_id = aws_route53_zone.scottmuc_com.zone_id
   name = aws_route53_zone.scottmuc_com.name
   type = "A"
   ttl = "3600"
   records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+}
+
+
+resource "gandi_zonerecord" "at_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "@"
+  type = "A"
+  ttl = "3600"
+  values = [
     "185.199.108.153",
     "185.199.109.153",
     "185.199.110.153",
@@ -35,6 +54,23 @@ resource "aws_route53_record" "mx-scottmuc_com" {
 }
 
 
+resource "gandi_zonerecord" "mx_at_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "@"
+  type = "MX"
+  ttl = "3600"
+  values = [
+    "90 aspmx2.google.com.",
+    "90 aspmx3.google.com.",
+    "50 alt1.aspmx.l.google.com.",
+    "10 aspmx.l.google.com.",
+    "50 alt2.aspmx.l.google.com.",
+    "90 aspmx4.google.com.",
+    "90 aspmx5.google.com."
+  ]
+}
+
+
 resource "aws_route53_record" "www_scottmuc_com" {
   zone_id = aws_route53_zone.scottmuc_com.zone_id
   name = "www.${aws_route53_zone.scottmuc_com.name}"
@@ -44,12 +80,33 @@ resource "aws_route53_record" "www_scottmuc_com" {
 }
 
 
+resource "gandi_zonerecord" "www_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "www"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "scottmuc.com."
+  ]
+}
+
 resource "aws_route53_record" "feeds_scottmuc_com" {
   zone_id = aws_route53_zone.scottmuc_com.zone_id
   name = "feeds.${aws_route53_zone.scottmuc_com.name}"
   type = "CNAME"
   ttl = "3600"
   records = ["18y3nfm.feedproxy.ghs.google.com."]
+}
+
+
+resource "gandi_zonerecord" "feeds_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "feeds"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "18y3nfm.feedproxy.ghs.google.com."
+  ]
 }
 
 
@@ -61,3 +118,19 @@ resource "aws_route53_record" "mail_scottmuc_com" {
   records = ["ghs.google.com."]
 }
 
+
+resource "gandi_zonerecord" "mail_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "mail"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "ghs.google.com."
+  ]
+}
+
+
+resource "gandi_domainattachment" "scotttmuc_com" {
+  zone   = data.gandi_zone.scottmuc_com.id
+  domain = "scottmuc.com"
+}
