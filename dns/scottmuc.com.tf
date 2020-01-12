8da3fdfb879603aa75bf7ba227eaec91311947ec
Scott Muc <scottmuc@gmail.com>
2020-01-12 18:07:08 +0100

    Add record for home raspberry pi

--- dns/scottmuc.com.tf
+++ dns/scottmuc.com.tf
@@ -143,6 +143,15 @@ resource "gandi_zonerecord" "home_scottmuc_com" {
   ]
 }
 
+resource "gandi_zonerecord" "pi_home_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "pi.home"
+  type = "A"
+  ttl = "3600"
+  values = [
+    "192.168.2.111"
+  ]
+}
 
 
 resource "gandi_domainattachment" "scotttmuc_com" {
