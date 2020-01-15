27d81d86bb0549e8603a76f2f9fa1710e760e322
Scott Muc <scottmuc@gmail.com>
2020-01-15 18:16:59 +0100

    Removed all AWS integration

--- dns/init.tf
+++ dns/init.tf
@@ -1,18 +1,6 @@
 variable "gandi_api_key" {
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
-
 provider "gandi" {
   key = var.gandi_api_key
-}
-
-terraform {
-  backend "s3" {
-    bucket = "smuc-terraform-state-files"
-    key = "dns.terraform.tfstate"
-    region = "eu-central-1"
-  }
 }
