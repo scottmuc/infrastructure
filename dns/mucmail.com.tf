90317fce6bbcb408332130fb1c8c7628af463a01
Scott Muc <scottmuc@gmail.com>
2020-01-15 17:59:23 +0100

    Removed mucmail.com from Route 53

--- dns/mucmail.com.tf
+++ dns/mucmail.com.tf
@@ -1,29 +1,7 @@
-
-resource "aws_route53_zone" "mucmail_com" {
-  name = "mucmail.com."
-}
-
-
 data "gandi_zone" "mucmail_com" {
   name = "mucmail.com"
 }
 
-
-resource "aws_route53_record" "mx-mucmail_com" {
-  zone_id = aws_route53_zone.mucmail_com.zone_id
-  name = aws_route53_zone.mucmail_com.name
-  type = "MX"
-  ttl = "28800"
-  records = [
-    "1 ASPMX.L.GOOGLE.COM.",
-    "3 ALT1.ASPMX.L.GOOGLE.COM.",
-    "3 ALT2.ASPMX.L.GOOGLE.COM.",
-    "5 ASPMX2.GOOGLEMAIL.COM.",
-    "5 ASPMX3.GOOGLEMAIL.COM.",
-    "5 ASPMX4.GOOGLEMAIL.COM",
-    "5 ASPMX5.GOOGLEMAIL.COM."
-  ]
-}
 
 resource "gandi_zonerecord" "mx_at_mucmail_com" {
   zone = data.gandi_zone.mucmail_com.id
