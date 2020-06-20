6c698e1b967cc81664585a5f7205f83f9d09e92f
Scott Muc <scottmuc@gmail.com>
2020-06-20 13:42:38 +0200

    Added records for protonmail
    Fixes #8

--- dns/scottmuc.com.tf
+++ dns/scottmuc.com.tf
@@ -27,7 +27,65 @@ resource "gandi_zonerecord" "txt_scottmuc_com" {
   type = "TXT"
   ttl = "3600"
   values = [
-    "keybase-site-verification=WYe5ju0VjNIRHhBkHg6s_ERw-CQBJ_LKq6zwfMDO5Wk"
+    "keybase-site-verification=WYe5ju0VjNIRHhBkHg6s_ERw-CQBJ_LKq6zwfMDO5Wk",
+    "protonmail-verification=18a56110687f875948252dca450d300f0b9dd2e6",
+    "v=spf1 include:_spf.protonmail.ch mx ~all"
+  ]
+}
+
+
+resource "gandi_zonerecord" "dmafc_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "_dmafc"
+  type = "TXT"
+  ttl = "3600"
+  values = [
+    "v=DMARC1; p=none; rua=mailto:scott@scottmuc.com"
+  ]
+}
+
+
+resource "gandi_zonerecord" "mx_at_scottmuc_com" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "@"
+  type = "MX"
+  ttl = "3600"
+  values = [
+    "10 mail.protonmail.ch.",
+    "20 mailsec.protonmail.ch."
+  ]
+}
+
+
+resource "gandi_zonerecord" "protonmail_domainkey" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "protonmail._domainkey"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "protonmail._domainkey.d5vyhyqcfvl4w4haoex2s3irbb6lx4r5xjqanoiwt2ppoguay27za.domains.proton.ch."
+  ]
+}
+
+
+resource "gandi_zonerecord" "protonmail2_domainkey" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "protonmail2._domainkey"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "protonmail2._domainkey.d5vyhyqcfvl4w4haoex2s3irbb6lx4r5xjqanoiwt2ppoguay27za.domains.proton.ch."
+  ]
+}
+
+
+resource "gandi_zonerecord" "protonmail3_domainkey" {
+  zone = data.gandi_zone.scottmuc_com.id
+  name = "protonmail3._domainkey"
+  type = "CNAME"
+  ttl = "3600"
+  values = [
+    "protonmail3._domainkey.d5vyhyqcfvl4w4haoex2s3irbb6lx4r5xjqanoiwt2ppoguay27za.domains.proton.ch."
   ]
 }
 
@@ -42,6 +100,7 @@ resource "gandi_zonerecord" "www_scottmuc_com" {
   ]
 }
 
+
 resource "gandi_zonerecord" "feeds_scottmuc_com" {
   zone = data.gandi_zone.scottmuc_com.id
   name = "feeds"
@@ -51,7 +110,6 @@ resource "gandi_zonerecord" "feeds_scottmuc_com" {
     "18y3nfm.feedproxy.ghs.google.com."
   ]
 }
-
 
 
 resource "gandi_zonerecord" "home_scottmuc_com" {
@@ -64,6 +122,7 @@ resource "gandi_zonerecord" "home_scottmuc_com" {
   ]
 }
 
+
 resource "gandi_zonerecord" "pi_home_scottmuc_com" {
   zone = data.gandi_zone.scottmuc_com.id
   name = "pi.home"
@@ -75,7 +134,7 @@ resource "gandi_zonerecord" "pi_home_scottmuc_com" {
 }
 
 
-resource "gandi_domainattachment" "scotttmuc_com" {
+resource "gandi_domainattachment" "scottmuc_com" {
   zone   = data.gandi_zone.scottmuc_com.id
   domain = "scottmuc.com"
 }
