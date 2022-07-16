91c98ddec6a3fc7e0a1d123534edc1ad9920cfcd
Scott Muc <scott@scottmuc.com>
2022-07-16 09:57:44 +0200

    Extract ffmpeg install to ansible

--- pi/navidrome.sh
+++ pi/navidrome.sh
@@ -4,14 +4,9 @@ service navidrome stop
 
 navidrome_version="0.47.5"
 navidrome_release_url="https://github.com/deluan/navidrome/releases/download/v${navidrome_version}/navidrome_${navidrome_version}_Linux_armv7.tar.gz"
-ffmpeg_download_url="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-static.tar.xz"
 
 curl -L "${navidrome_release_url}" 2> /dev/null \
   | tar xzv -C /opt/navidrome
-
-(cd /tmp && curl -LO "${ffmpeg_download_url}" 2> /dev/null)
-
-tar xvf /tmp/ffmpeg-release-armhf-static.tar.xz --strip-components=1 -C /opt/navidrome/ffmpeg
 
 cat > /opt/navidrome/navidrome.toml <<EOF
 LogLevel = "INFO"
