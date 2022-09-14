3b70db55a724f93512c522ed3fea7241c187e017
Scott Muc <scott@scottmuc.com>
2022-09-14 13:30:38 +0200

    Replace echo with gum confirm message

--- pi/ansible-gum.sh
+++ pi/ansible-gum.sh
@@ -9,8 +9,7 @@ if [ "${ip}" = "other" ]; then
   ip=$(gum input --placeholder "192.168.2.x")
 fi
 
-echo "deploying ${playbook} to ${ip}"
-gum confirm || exit 1
+gum confirm "Deploy ${playbook} to ${ip}?" || exit 1
 
 ansible-playbook \
   --extra-vars "ansible_python_interpreter=/usr/bin/python3" \
