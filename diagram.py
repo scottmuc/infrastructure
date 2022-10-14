2ebc91d225e91e267f82a245de8d620586ee28e7
Scott Muc <scott@scottmuc.com>
2022-10-14 15:23:54 +0200

    Update network diagram

--- diagram.py
+++ diagram.py
@@ -1,7 +1,26 @@
 from diagrams import Cluster, Diagram
 from diagrams.onprem.monitoring import Grafana, Prometheus
+from diagrams.generic.network import Router, Switch
+from diagrams.generic.os import Windows, LinuxGeneral
+from diagrams.onprem.logging import Rsyslog
 
 with Diagram("Home Network", show=False):
+  router = Router("Telekom Router")
+
+  with Cluster("TV Stand"):
+    tv_switch = Switch("TV Netgear Switch")
+
+  with Cluster("Desktop"):
+    desktop_switch = Switch("Desktop Netgear Switch")
+    desktop = Windows("Desktop PC")
+
   with Cluster("Raspberry PI"):
-    metrics = Prometheus("metrics")
-    metrics << Grafana("monitoring")
+    pi = LinuxGeneral("Debian")
+    Prometheus("metrics")
+    Grafana("monitoring")
+    Rsyslog("log forwarding")
+
+  pi >> router
+  desktop_switch >> router
+  tv_switch >> desktop_switch
+  desktop >> desktop_switch
