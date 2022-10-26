b3927166f9f143104cf06240698d5f9e2c744b13
Scott Muc <scott@scottmuc.com>
2022-10-26 16:42:56 +0200

    Upgrade gum to 0.8.0

--- homedirs/common/lib/install_gum.bash
+++ homedirs/common/lib/install_gum.bash
@@ -1,5 +1,13 @@
 
 install_gum() {
-  curl -L "https://github.com/charmbracelet/gum/releases/download/v0.6.0/gum_0.6.0_$(uname)_x86_64.tar.gz" 2> /dev/null \
+  local version
+
+  version="${1:-0.6.0}"
+
+  if command -v gum &> /dev/null; then
+    echo "gum exists: $(gum --version)"
+    return
+  fi
+  curl -L "https://github.com/charmbracelet/gum/releases/download/v${version}/gum_${version}_$(uname)_x86_64.tar.gz" 2> /dev/null \
     | tar -zxvf - -C ../../vendor/bin gum
 }
