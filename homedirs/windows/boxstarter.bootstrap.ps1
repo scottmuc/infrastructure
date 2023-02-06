f97d75d21de74cccaaa8162f5e94acd2a74c5264
Scott Muc <scott@scottmuc.com>
2023-02-06 17:46:59 +0100

    Add RefreshEnv so git command exists in PATH
    Discovered in #47

--- homedirs/windows/boxstarter.bootstrap.ps1
+++ homedirs/windows/boxstarter.bootstrap.ps1
@@ -4,4 +4,8 @@ choco install git -y --source="'https://chocolatey.org/api/v2'" `
 $Workspace = (Join-Path -Path $Env:USERPROFILE -Child workspace)
 New-Item -ItemType "directory" -Path $Workspace -Force
 
+# Very nifty command from Chocolatey:
+# https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/redirects/RefreshEnv.cmd
+RefreshEnv
+
 git clone https://github.com/scottmuc/infrastructure.git $(Join-Path -Path $Workspace -ChildPath infrastructure)
