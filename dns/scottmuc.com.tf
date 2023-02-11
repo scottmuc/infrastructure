29eb42dee22e88a922286a844cecc3146b795176
Kool User <lx4r@users.noreply.github.com>
2023-02-11 13:49:47 +0100

    Test terraform on new WSL instance
    Part of #48
    
    Signed-off-by: Scott Muc <scott@scottmuc.com>

--- dns/scottmuc.com.tf
+++ dns/scottmuc.com.tf
@@ -160,5 +160,6 @@ resource "gandi_livedns_record" "graffiti_scottmuc_com" {
   values = [
     "\"WSL repaved on Apr 18 2022. Cleaned up the historic graffiti\"",
     "\"WSL repaved on Nov 6 2022. Will O'Leary was here\"",
+    "\"WSL repaved on Feb 11 2022. Kool User was here\"",
   ]
 }
