a8e6afab7e9185a211110c90ad7d3aed5a4adeed
Scott Muc <scott@scottmuc.com>
2023-05-14 17:37:35 +0200

    Use Debloat-Windows-10 apps removal code

--- homedirs/windows/boxstarter.ps1
+++ homedirs/windows/boxstarter.ps1
@@ -1,5 +1,6 @@
 
 function Invoke-Main {
+    Remove-DefaultApplications
     Enable-CouchGamingOnStartup
     Enable-DarkThemeToggler
     Enable-GitConfig
@@ -8,66 +9,140 @@ function Invoke-Main {
     Set-ThisDirectoryToQuickAccess
 }
 
-# Taken from https://gist.github.com/bitcrazed/c788f9dcf1d630340a19
-#--- Used to uninstall unwanted default apps ---
 
-function Remove-App
-{
-    Param ([string]$appName)
-    Write-Output "Trying to remove $appName"
-    Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
-    Get-AppXProvisionedPackage -Online `
-        | Where-Object DisplayName -like $appName `
-        | Remove-AppxProvisionedPackage -Online
-}
-
-#--- Uninstall unwanted default apps ---
-
-# Get a list of existing applications by running:
-#
-# Get-AppxPackage | % { $_.Name }
-# 
-# and https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
-$applicationList = @(
-    "Microsoft.BingFinance"
-    "Microsoft.3DBuilder"
-    "Microsoft.CommsPhone"
-    "Microsoft.Getstarted"
-    "*MarchofEmpires*"
-    "Microsoft.GetHelp"
-    "Microsoft.Messaging"
-    "*Minecraft*"
-    "Microsoft.MicrosoftOfficeHub"
-    "Microsoft.WindowsPhone"
-    "*Solitaire*"
-    "Microsoft.MicrosoftStickyNotes"
-    "Microsoft.Office.Sway"
-
-    "Microsoft.XboxApp"
-    "Microsoft.XboxIdentityProvider"
-
-    "Microsoft.NetworkSpeedTest"
-    "Microsoft.Print3D"
-
-    #Non-Microsoft
-
-    "*Autodesk*"
-    "*BubbleWitch*"
-    "king.com.CandyCrush*"
-    "*Dell*"
-    "*Dropbox*"
-    "*Facebook*"
-    "*Keeper*"
-    "*Twitter*"
-    "*Plex*"
-    "*.Duolingo-LearnLanguagesforFree"
-    "*.EclipseManager"
-    "ActiproSoftwareLLC.562882FEEB491" # Code Writer
-    "*.AdobePhotoshopExpress");
-
-
-foreach ($app in $applicationList) {
-    Remove-App $app
+# Origin https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
+function Remove-DefaultApplications {
+    $apps = @(
+        # default Windows 10 apps
+        "Microsoft.549981C3F5F10" #Cortana
+        "Microsoft.3DBuilder"
+        "Microsoft.Appconnector"
+        "Microsoft.BingFinance"
+        "Microsoft.BingNews"
+        "Microsoft.BingSports"
+        "Microsoft.BingTranslator"
+        "Microsoft.BingWeather"
+        #"Microsoft.FreshPaint"
+        "Microsoft.GamingServices"
+        "Microsoft.MicrosoftOfficeHub"
+        "Microsoft.MicrosoftPowerBIForWindows"
+        "Microsoft.MicrosoftSolitaireCollection"
+        #"Microsoft.MicrosoftStickyNotes"
+        "Microsoft.MinecraftUWP"
+        "Microsoft.NetworkSpeedTest"
+        "Microsoft.Office.OneNote"
+        "Microsoft.People"
+        "Microsoft.Print3D"
+        "Microsoft.SkypeApp"
+        "Microsoft.Wallet"
+        #"Microsoft.Windows.Photos"
+        "Microsoft.WindowsAlarms"
+        #"Microsoft.WindowsCalculator"
+        "Microsoft.WindowsCamera"
+        "microsoft.windowscommunicationsapps"
+        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsPhone"
+        "Microsoft.WindowsSoundRecorder"
+        #"Microsoft.WindowsStore"   # can't be re-installed
+        "Microsoft.Xbox.TCUI"
+        "Microsoft.XboxApp"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.YourPhone"
+        "Microsoft.ZuneMusic"
+        "Microsoft.ZuneVideo"
+    
+        # Threshold 2 apps
+        "Microsoft.CommsPhone"
+        "Microsoft.ConnectivityStore"
+        "Microsoft.GetHelp"
+        "Microsoft.Getstarted"
+        "Microsoft.Messaging"
+        "Microsoft.Office.Sway"
+        "Microsoft.OneConnect"
+        "Microsoft.WindowsFeedbackHub"
+    
+        # Creators Update apps
+        "Microsoft.Microsoft3DViewer"
+        #"Microsoft.MSPaint"
+    
+        #Redstone apps
+        "Microsoft.BingFoodAndDrink"
+        "Microsoft.BingHealthAndFitness"
+        "Microsoft.BingTravel"
+        "Microsoft.WindowsReadingList"
+    
+        # Redstone 5 apps
+        "Microsoft.MixedReality.Portal"
+        "Microsoft.ScreenSketch"
+        "Microsoft.XboxGamingOverlay"
+    
+        # non-Microsoft
+        "2FE3CB00.PicsArt-PhotoStudio"
+        "46928bounde.EclipseManager"
+        "4DF9E0F8.Netflix"
+        "613EBCEA.PolarrPhotoEditorAcademicEdition"
+        "6Wunderkinder.Wunderlist"
+        "7EE7776C.LinkedInforWindows"
+        "89006A2E.AutodeskSketchBook"
+        "9E2F88E3.Twitter"
+        "A278AB0D.DisneyMagicKingdoms"
+        "A278AB0D.MarchofEmpires"
+        "ActiproSoftwareLLC.562882FEEB491" # next one is for the Code Writer from Actipro Software LLC
+        "CAF9E577.Plex"  
+        "ClearChannelRadioDigital.iHeartRadio"
+        "D52A8D61.FarmVille2CountryEscape"
+        "D5EA27B7.Duolingo-LearnLanguagesforFree"
+        "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+        "DolbyLaboratories.DolbyAccess"
+        "DolbyLaboratories.DolbyAccess"
+        "Drawboard.DrawboardPDF"
+        "Facebook.Facebook"
+        "Fitbit.FitbitCoach"
+        "Flipboard.Flipboard"
+        "GAMELOFTSA.Asphalt8Airborne"
+        "KeeperSecurityInc.Keeper"
+        "NORDCURRENT.COOKINGFEVER"
+        "PandoraMediaInc.29680B314EFC2"
+        "Playtika.CaesarsSlotsFreeCasino"
+        "ShazamEntertainmentLtd.Shazam"
+        "SlingTVLLC.SlingTV"
+        "SpotifyAB.SpotifyMusic"
+        #"TheNewYorkTimes.NYTCrossword"
+        "ThumbmunkeysLtd.PhototasticCollage"
+        "TuneIn.TuneInRadio"
+        "WinZipComputing.WinZipUniversal"
+        "XINGAG.XING"
+        "flaregamesGmbH.RoyalRevolt2"
+        "king.com.*"
+        "king.com.BubbleWitch3Saga"
+        "king.com.CandyCrushSaga"
+        "king.com.CandyCrushSodaSaga"
+        "A025C540.Yandex.Music"
+    
+        # apps which cannot be removed using Remove-AppxPackage
+        #"Microsoft.BioEnrollment"
+        #"Microsoft.MicrosoftEdge"
+        #"Microsoft.Windows.Cortana"
+        #"Microsoft.WindowsFeedback"
+        #"Microsoft.XboxGameCallableUI"
+        #"Microsoft.XboxIdentityProvider"
+        #"Windows.ContactSupport"
+    
+        # apps which other apps depend on
+        "Microsoft.Advertising.Xaml"
+    )
+    
+    $appxprovisionedpackage = Get-AppxProvisionedPackage -Online
+    
+    foreach ($app in $apps) {
+        Write-Output "Trying to remove $app"
+    
+        Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+    
+        ($appxprovisionedpackage).Where( {$_.DisplayName -EQ $app}) |
+            Remove-AppxProvisionedPackage -Online
+    }
 }
 
 # https://boxstarter.org/winconfig#set-windowsexploreroptions
