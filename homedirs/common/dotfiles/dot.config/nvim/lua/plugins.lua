c5897c0490a0c1ee1e059585c54f2a9499e68709
Scott Muc <scott@scottmuc.com>
2023-08-09 15:32:45 +0200

    Add telescope to nvim configuration
    Issue: #63

--- homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
@@ -5,4 +5,9 @@ return require('packer').startup(function(use)
 
   use 'tpope/vim-sleuth'
   use 'overcache/NeoSolarized'
+
+  use {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
