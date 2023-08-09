3d23bd0431533a3a67f5d98be3ac71b751589d21
Scott Muc <scott@scottmuc.com>
2023-08-09 17:13:29 +0200

    Add treesitter
    Issue: #63

--- homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
@@ -10,4 +10,12 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
 end)
