79dc1f1b4ce10000c6c752b17daa74232fa3de49
Scott Muc <scott@scottmuc.com>
2023-08-09 17:23:05 +0200

    Add LSP support
    Issue: #63

--- homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/plugins.lua
@@ -18,4 +18,27 @@ return require('packer').startup(function(use)
       ts_update()
     end,
   }
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {'williamboman/mason.nvim'},           -- Optional
+		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},         -- Required
+		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+		  {'hrsh7th/cmp-buffer'},       -- Optional
+		  {'hrsh7th/cmp-path'},         -- Optional
+		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},             -- Required
+		  {'rafamadriz/friendly-snippets'}, -- Optional
+	  }
+  }
 end)
