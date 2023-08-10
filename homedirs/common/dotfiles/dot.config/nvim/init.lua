f9868fc39c942147887719c09f4af3a5acac2b8f
Scott Muc <scott@scottmuc.com>
2023-08-10 11:04:31 +0200

    Add and learn some nice keymaps
    Grabbed a few from ThePrimeagen. After practicing them a few times I
    found they work really well and aren't too obscure to understand.
    
    Going to experiment with <Space> as my leader key. Felt natural already
    while composing the contents of this commit.
    
    Issue: #63

--- homedirs/common/dotfiles/dot.config/nvim/init.lua
+++ homedirs/common/dotfiles/dot.config/nvim/init.lua
@@ -1,6 +1,21 @@
 require('plugins')
 
-vim.g.mapleader = ","
+-- some inspiration for these settings are comming from:
+-- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
+vim.g.mapleader = " "
+
+-- use netrw as a NerdTree replacement
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- move blocks of text visually
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- when joining, don't change where the cursor sits
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- replace without entering the replaced text in the default register
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.opt.number = true -- Enable line numbers. This is off by default
 vim.opt.relativenumber = true
@@ -15,6 +30,8 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
 
 vim.cmd([[
 :silent! colorscheme NeoSolarized
