2c74a049775258eabdd8c4dd6f36371590f1fac8
Scott Muc <scott@scottmuc.com>
2024-03-17 15:28:21 +0100

    Add bash language server

--- homedirs/common/dotfiles/dot.config/nvim-kickstart/init.lua
+++ homedirs/common/dotfiles/dot.config/nvim-kickstart/init.lua
@@ -539,6 +539,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        bashls = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
