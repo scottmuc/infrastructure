cf5bf9fcee88a44bc85450c35348ed4a717e7c14
Scott Muc <scott@scottmuc.com>
2024-05-27 08:15:36 +0200

    Add fail2ban to the server
    Issue: #71

--- pi/ansible.sh
+++ pi/ansible.sh
@@ -9,6 +9,7 @@ playbook=$(find . -name "*playbook.yml" \
 tags=$(gum choose --header "Select tag to apply" \
   "all"           \
   "certbot"       \
+  "fail2ban"      \
   "git"           \
   "grafana"       \
   "logging"       \
