d82f6aa24ca5e3374024bbfd6901bac71cf109bb
Scott Muc <scott@scottmuc.com>
2024-06-15 11:11:40 +0200

    Add nvim-tree, I still like tree based browsing

--- homedirs/common/dotfiles/dot.config/nvim/lua/keymaps.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/keymaps.lua
@@ -11,3 +11,5 @@ k.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 k.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 k.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 k.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+k.set('n', '\\', ':NvimTreeToggle<cr>', { silent = true, noremap = true })
