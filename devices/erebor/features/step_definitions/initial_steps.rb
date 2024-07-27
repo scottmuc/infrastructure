dfa975f587d76dc79ba226122a8b3308188629f0
Scott Muc <scott@scottmuc.com>
2024-07-27 12:00:01 +0200

    Initial setup and restore sucessful!

--- devices/erebor/features/step_definitions/initial_steps.rb
+++ devices/erebor/features/step_definitions/initial_steps.rb
@@ -7,6 +7,10 @@ class ZfsFixture
     Vagrant.new.exec "sudo mdconfig -u #{name} -f #{name}"
   end
 
+  def damage_drive(name)
+    Vagrant.new.exec "dd if=/dev/zero of=#{name} bs=4M count=1"
+  end
+
   def delete_drive(name)
     Vagrant.new.exec "sudo mdconfig -du #{name}"
     Vagrant.new.exec "rm #{name}"
@@ -46,19 +50,42 @@ end
 # Thanks to this post as a great reference:
 # https://wiki.archlinux.org/title/ZFS/Virtual_disks
 And('it contains some data') do
-  Vagrant.new.exec "sudo fetch https://www.gutenberg.org/ebooks/2701.txt.utf-8 -o /testpool/Moby_Dick.txt"
-  output = Vagrant.new.exec "cat /testpool/Moby_Dick.txt | grep Melville"
-  expect(output).to match(/Melvill/)
+  # Vagrant.new.exec "sudo cp /vagrant/features/data/*.txt /testpool"
+  # output = Vagrant.new.exec "cat /testpool/Moby_Dick.txt | grep Melville"
+  # expect(output).to match(/Melville/)
 end
 
 Given('that one of the disks has failed') do
-  pending # Write code here that turns the phrase above into concrete actions
+  ZfsFixture.new.damage_drive "md0"
+  Vagrant.new.exec "sudo zpool scrub testpool"
+  output = Vagrant.new.exec "zpool status testpool"
+  # pool: testpool
+  #  state: DEGRADED
+  # status: One or more devices could not be used because the label is missing or
+  #  invalid.  Sufficient replicas exist for the pool to continue
+  #  functioning in a degraded state.
+  # action: Replace the device using 'zpool replace'.
+  #    see: https://openzfs.github.io/openzfs-docs/msg/ZFS-8000-4J
+  #   scan: scrub repaired 0B in 00:00:00 with 0 errors on Sat Jul 27 09:49:17 2024
+  # config:
+  #
+  #  NAME        STATE     READ WRITE CKSUM
+  #  testpool    DEGRADED     0     0     0
+  #    raidz1-0  DEGRADED     0     0     0
+  #      md0     UNAVAIL      4     0     0  invalid label
+  #      md1     ONLINE       0     0     0
+  #      md2     ONLINE       0     0     0
+  expect(output).to match(/state: DEGRADED/)
 end
 
 When('the failed disk is replaced') do
-  pending # Write code here that turns the phrase above into concrete actions
+  ZfsFixture.new.create_drive "md3"
+  Vagrant.new.exec "sudo zpool replace testpool /dev/md0 /dev/md3"
 end
 
 Then('my files are all still available') do
-  pending # Write code here that turns the phrase above into concrete actions
+  output = Vagrant.new.exec "zpool status testpool"
+  expect(output).to match(/state: ONLINE/)
+  # output = Vagrant.new.exec "cat /testpool/Moby_Dick.txt | grep Melville"
+  # expect(output).to match(/Melville/)
 end
