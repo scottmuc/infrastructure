e55f7af3843773ea972b6314159f0f990aec96f4
Scott Muc <scott@scottmuc.com>
2024-08-25 14:27:30 +0200

    No need for asdf installer as a lib

--- homedirs/common/lib/install_asdf.bash
+++
@@ -1,8 +0,0 @@
-install_asdf() {
-  if command -v asdf >/dev/null; then
-    echo "asdf exists: $(asdf --version)"
-    return
-  fi
-
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
-}
