aac1465a15fcf0e16d68346b28b4f161a0d304ff
Scott Muc <scott@scottmuc.com>
2025-01-20 22:03:07 +0100

    Add a few more packages and explanatory comments
    - Clean up a bunch of comments that I no longer need for comprehension.
    
    Issue: #84

--- homedirs/common/dotfiles/dot.config/home-manager/home.nix
+++ homedirs/common/dotfiles/dot.config/home-manager/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "smootz";
   home.homeDirectory = "/home/smootz";
 
@@ -18,12 +16,17 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    # Revert back to bash for daily terminal use.
     pkgs.bash
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
-    # The nix LSP, not the Determinate Systems auth helper nor the crypto thingy
+    # Going to continue to use mise for project level dependency management. Not going to
+    # impose Nix on anyone at this point.
+    pkgs.mise
+
+    # Mods is a TUI for ChatGPT like functionality.
+    pkgs.mods
+
+    # The nix LSP, not the Determinate Systems auth helper nor the crypto thingy.
     pkgs.nixd
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
@@ -55,24 +58,10 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/smootz/etc/profile.d/hm-session-vars.sh
-  #
+  # Variables defined here will show up in `.nix-profile/etc/profile.d/hm-session-vars.sh`,
+  # which is sourced by `~/.profile`.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
