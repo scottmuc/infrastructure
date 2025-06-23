9637149987100722fdf43c456269600f139ba51b
Scott Muc <scott@scottmuc.com>
2025-06-23 20:56:41 +0200

    Check for playwright browsers
    Use vertical spacing for args to cucumber-js execution
    
    Issue: #91

--- devices/pippin/tests/test.sh
+++ devices/pippin/tests/test.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 
 main() {
     check_node
+    check_playwright
 
     # If we've reached here, all machine dependecnies are met!
     install_dependencies
@@ -11,7 +12,10 @@ main() {
 }
 
 run_tests() {
-    ./node_modules/.bin/cucumber-js
+    npx cucumber-js \
+      features/*.feature \
+      --require-module ts-node/register \
+      --require steps/*.ts
 }
 
 install_dependencies() {
@@ -25,6 +29,14 @@ check_node() {
     fi
     echo "Node JS location : $(command -v node)"
     echo "Node JS version  : $(node --version)"
+}
+
+check_playwright() {
+    browser_count="$(ls ~/.cache/ms-playwright/ | wc -l)"
+    if (( browser_count == 0 )); then
+        echo "Playwright browsers not installed, run 'npx playwright install'"
+        exit 1
+    fi
 }
 
 main "$@"
