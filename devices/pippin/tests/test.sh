7d6192177dcbaec345a87610a200cd0bd5c7e4d3
Scott Muc <scott@scottmuc.com>
2025-07-17 22:46:43 +0200

    Looks like we still need up install browsers

--- devices/pippin/tests/test.sh
+++ devices/pippin/tests/test.sh
@@ -8,7 +8,16 @@ main() {
 
     # If we've reached here, all machine dependecnies are met!
     install_dependencies
+
+    if [[ "${NAVIDROME_TEST_ENVIRONMENT}" != "local" ]]; then
+      install_playwright_browsers
+    fi
+
     run_tests
+}
+
+install_playwright_browsers() {
+  npx playwright install
 }
 
 run_tests() {
