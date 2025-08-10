9311efc4cc033f61f12f9312119ea0a0e1c10022
Scott Muc <scott@scottmuc.com>
2025-08-10 11:30:39 +0200

    Trying a higher contrast colour scheme
    This is working well with alabaster with alacritty. May try cyberdream
    with alacritty too.

--- homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/colorschems.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/colorschems.lua
@@ -1,14 +1,10 @@
 return {
-  'folke/tokyonight.nvim',
-
   {
     'scottmckendry/cyberdream.nvim',
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'tokyonight-moon'
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'cyberdream-light'
     end,
   },
 }
