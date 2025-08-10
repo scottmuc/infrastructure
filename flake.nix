5947bad941c03f437c8e2fd0837d386782d27cf5
Scott Muc <scott@scottmuc.com>
2025-08-10 22:09:30 +0200

    Add a flake updater that prints out a diff

--- flake.nix
+++ flake.nix
@@ -2,22 +2,25 @@
   description = "Mucrastructure Dev Shell";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.ansible
-        pkgs.bash
-        pkgs.fly
-        pkgs.git-crypt
-        pkgs.opentofu
-        pkgs.shellcheck
-      ];
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.ansible
+          pkgs.bash
+          pkgs.fly
+          pkgs.git-crypt
+          pkgs.opentofu
+          pkgs.shellcheck
+        ];
+      };
     };
-  };
 }
