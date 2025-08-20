ffb1c9edd126d85d1a6d3e7c737dbddfb7864d15
Scott Muc <scott@scottmuc.com>
2025-08-20 20:11:32 +0200

    Move page models to their own file
    Issue: #91

---
+++ devices/pippin/tests/page-models.ts
@@ -0,0 +1,70 @@
+import { Page, Browser } from "playwright";
+import { expect, Locator } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly url: string;
+
+  constructor(baseUrl: string, page: Page) {
+    this.page = page;
+    this.url = constructUrl(baseUrl, "/app/#/login");
+  }
+
+  async goto() {
+    await this.page.goto(this.url);
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill("input[name='username']", username);
+    await this.page.fill("input[name='password']", password);
+    await this.page.click("button[type='submit']");
+    await this.page.waitForTimeout(500);
+    expect(this.page.url()).toContain(
+      "/app/#/album/recentlyAdded?sort=recently_added&order=DESC&filter={}"
+    );
+  }
+}
+
+export class HomePage {
+  readonly page: Page;
+  readonly url: string;
+  readonly titleSelector: string;
+
+  constructor(baseUrl: string, page: Page) {
+    this.page = page;
+    this.url = constructUrl(baseUrl, "app/#/album/recentlyAdded");
+    this.titleSelector = "#react-admin-title";
+  }
+
+  get titleLocator(): Locator {
+    return this.page.locator(this.titleSelector);
+  }
+}
+
+const constructUrl = (base: string, path: string): string => {
+  // OMG, the URL object will squash the path in the base if
+  // the path variable begins with a '/'. Rather than ensuring
+  // someone specifies a base WITH a trailing '/' and ensuring
+  // someone specifies a path WITHOUT a leading '/', we do some
+  // some sanitization of both params to ensure all the following
+  // work:
+  //
+  // base: https://home.scottmuc.com/music
+  // path: app/#/login
+  //  out: https://home.scottmuc.com/music/app/#/login
+  //
+  // base: https://home.scottmuc.com/music
+  // path: /app/#/login
+  //  out: https://home.scottmuc.com/music/app/#/login
+  //
+  // base: https://home.scottmuc.com/music/
+  // path: app/#/login
+  //  out: https://home.scottmuc.com/music/app/#/login
+  //
+  // base: https://home.scottmuc.com/music/
+  // path: /app/#/login
+  //  out: https://home.scottmuc.com/music/app/#/login
+  return new URL(path.replace(/^\/+/g, ""), base.replace(/\/+$/g, "") + "/")
+    .href;
+};
+
--- devices/pippin/tests/steps/play-a-song.ts
+++ devices/pippin/tests/steps/play-a-song.ts
@@ -10,81 +10,14 @@ import {
 import { chromium, Page, Browser } from "playwright";
 import { expect, Locator } from "@playwright/test";
 import { TestConfig } from "../test-config";
-
+import { HomePage, LoginPage } from "../page-models";
 // This must be called in a global scope, otherwise it gets reset.
 // https://github.com/cucumber/cucumber-js/blob/HEAD/docs/support_files/timeouts.md
 setDefaultTimeout(600000);
 
-export class LoginPage {
-  readonly page: Page;
-  readonly url: string;
-
-  constructor(baseUrl: string, page: Page) {
-    this.page = page;
-    this.url = constructUrl(baseUrl, "/app/#/login");
-  }
-
-  async goto() {
-    await this.page.goto(this.url);
-  }
-
-  async login(username: string, password: string) {
-    await this.page.fill("input[name='username']", username);
-    await this.page.fill("input[name='password']", password);
-    await this.page.click("button[type='submit']");
-    await this.page.waitForTimeout(500);
-    expect(this.page.url()).toContain(
-      "/app/#/album/recentlyAdded?sort=recently_added&order=DESC&filter={}"
-    );
-  }
-}
-
-class HomePage {
-  readonly page: Page;
-  readonly url: string;
-  readonly titleSelector: string;
-
-  constructor(baseUrl: string, page: Page) {
-    this.page = page;
-    this.url = constructUrl(baseUrl, "app/#/album/recentlyAdded");
-    this.titleSelector = "#react-admin-title";
-  }
-
-  get titleLocator(): Locator {
-    return this.page.locator(this.titleSelector);
-  }
-}
-
 let page: Page;
 let browser: Browser;
 let testConfig: TestConfig;
-
-const constructUrl = (base: string, path: string): string => {
-  // OMG, the URL object will squash the path in the base if
-  // the path variable begins with a '/'. Rather than ensuring
-  // someone specifies a base WITH a trailing '/' and ensuring
-  // someone specifies a path WITHOUT a leading '/', we do some
-  // some sanitization of both params to ensure all the following
-  // work:
-  //
-  // base: https://home.scottmuc.com/music
-  // path: app/#/login
-  //  out: https://home.scottmuc.com/music/app/#/login
-  //
-  // base: https://home.scottmuc.com/music
-  // path: /app/#/login
-  //  out: https://home.scottmuc.com/music/app/#/login
-  //
-  // base: https://home.scottmuc.com/music/
-  // path: app/#/login
-  //  out: https://home.scottmuc.com/music/app/#/login
-  //
-  // base: https://home.scottmuc.com/music/
-  // path: /app/#/login
-  //  out: https://home.scottmuc.com/music/app/#/login
-  return new URL(path.replace(/^\/+/g, ""), base.replace(/\/+$/g, "") + "/")
-    .href;
-};
 
 Before(async () => {
   testConfig = new TestConfig();
