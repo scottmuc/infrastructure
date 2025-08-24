d209c2c6ff4a0788063eeff117c405df01945dd0
Scott Muc <scott@scottmuc.com>
2025-08-24 22:32:45 +0200

    Attempting to build and publish an image

--- devices/pippin/tests/concourse/set-pipelines.sh
+++ devices/pippin/tests/concourse/set-pipelines.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 
 fly -t concourse.scottmuc.com \
   set-pipeline \
+  --pipeline docker-image \
+  --config ./docker-image.yml
+
+fly -t concourse.scottmuc.com \
+  set-pipeline \
   --pipeline pippin \
   --config ./end-to-end.yml
