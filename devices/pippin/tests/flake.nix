7b32094740b1a903f43da09babb76b4ae48e6b82
Scott Muc <scott@scottmuc.com>
2025-08-24 10:23:50 +0200

    Solved using nix to provider playwright browsers
    This requires that the package.json pins @playwright/test to the same
    version of the playwright-driver version, otherwise the browser versions
    that are expected by the test suite.

---
+++ devices/pippin/tests/flake.nix
@@ -0,0 +1,28 @@
+{
+  # https://wiki.nixos.org/wiki/Playwright
+  description = "Cucumber-js tests with Playwright";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+          pkgs.playwright-driver
+        ];
+
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        '';
+      };
+    };
+}
