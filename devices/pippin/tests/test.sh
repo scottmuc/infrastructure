5ab84c2cd3feb7eb549271424a09fc64975e8d2d
Scott Muc <scott@scottmuc.com>
2025-09-14 20:08:09 +0200

    Fix incoorect check for local execution of tests

--- devices/pippin/tests/test.sh
+++ devices/pippin/tests/test.sh
@@ -9,7 +9,7 @@ main() {
     # If we've reached here, all machine dependecnies are met!
     install_dependencies
 
-    if [[ "${NAVIDROME_TEST_ENVIRONMENT-:""}" != "local" ]]; then
+    if [[ "${NAVIDROME_TEST_ENVIRONMENT:-"local"}" != "local" ]]; then
       install_playwright_browsers
     fi
 
