d0ae30b8688df2ffa835bdf5a66a5188c5908552
Scott Muc <scott@scottmuc.com>
2025-10-26 21:43:55 +0100

    Add ansible stuff for erebor
    Issue: #78

---
+++ devices/erebor/ansible.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+set -e
+
+playbook=$(find ./playbooks/ -name "*.yml" \
+  | sort -r \
+  | gum choose --header "Select a playbook")
+
+tags=$(gum choose --header "Select tag to apply" \
+  "all"           \
+)
+
+inventory_choice=$(gum choose --header "Select inventory" \
+  "inventory file" \
+  "specific ip")
+
+inventory_arg="inventory.ini"
+if [ "${inventory_choice}" = "specific ip" ]; then
+  inventory_arg="$(gum input --placeholder "192.168.2.x"),"
+fi
+
+gum confirm "Deploy ${playbook} using ${inventory_choice}?" || exit 1
+
+set -x
+ansible-playbook \
+  --extra-vars "ansible_python_interpreter=/usr/bin/python3" \
+  --inventory "${inventory_arg}" \
+  --tags "${tags}" \
+  "${playbook}"
