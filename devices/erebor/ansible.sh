db192e5bba35bf14df3e476a99a37a13f71c370d
Scott Muc <scott@scottmuc.com>
2025-10-30 19:43:03 +0100

    Begin ansible automation
    Starting the playbook off by removing the bootstrap user
    
    Issue: #98

---
+++ devices/erebor/ansible.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -eu
+
+playbook=$(find ./playbooks/ -name "*.yml" \
+  | grep -v bootstrap \
+  | sort -r \
+  | gum choose --header "Select a playbook")
+
+gum confirm "Deploy ${playbook} to erebor?" || exit 1
+
+set -x
+ansible-playbook \
+  --extra-vars "ansible_python_interpreter=/usr/local/bin/python" \
+  --inventory "inventory.ini" \
+  --tags "all" \
+  "${playbook}"
