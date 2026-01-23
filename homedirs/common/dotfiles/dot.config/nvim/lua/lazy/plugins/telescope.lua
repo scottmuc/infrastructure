2469ca68953fb95541f5a56a87e3ab0cd003ef54
Scott Muc <scott@scottmuc.com>
2026-01-23 10:07:19 +0100

    Update telescope to version that works with new treesitter

--- homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/telescope.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/telescope.lua
@@ -8,7 +8,7 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
+  tag = 'v0.2.1',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for install instructions
