d63fa34cc87a8f7ac07877646369c13e1b0c0700
Scott Muc <scott@scottmuc.com>
2026-02-21 17:52:03 +0100

    Always use inventory for merry

--- devices/merry/ansible.sh
+++ devices/merry/ansible.sh
@@ -6,25 +6,11 @@ playbook=$(find ./playbooks/ -name "*.yml" \
   | sort -r \
   | gum choose --header "Select a playbook")
 
-tags=$(gum choose --header "Select tag to apply" \
-  "all"           \
-)
-
-inventory_choice=$(gum choose --header "Select inventory" \
-  "inventory file" \
-  "specific ip")
-
-inventory_arg="../inventory.yml"
-if [ "${inventory_choice}" = "specific ip" ]; then
-  inventory_arg="$(gum input --placeholder "192.168.2.x"),"
-fi
-
-gum confirm "Deploy ${playbook} using ${inventory_choice}?" || exit 1
+gum confirm "Run ${playbook}" || exit 1
 
 set -x
 env \
   ANSIBLE_CONFIG=../ansible.cfg \
 ansible-playbook \
-  --inventory "${inventory_arg}" \
-  --tags "${tags}" \
+  --inventory "../inventory.yml" \
   "${playbook}"
