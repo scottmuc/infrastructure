d71e1c87592411be733e65c2fa4b54d69b610aaf
Scott Muc <scott@scottmuc.com>
2026-02-28 22:46:07 +0100

    Remove old-school docker image build

--- devices/pippin/tests/concourse/set-pipelines.sh
+++ devices/pippin/tests/concourse/set-pipelines.sh
@@ -4,10 +4,5 @@ set -euo pipefail
 
 fly -t concourse.scottmuc.com \
   set-pipeline \
-  --pipeline docker-image \
-  --config ./docker-image.yml
-
-fly -t concourse.scottmuc.com \
-  set-pipeline \
   --pipeline pippin \
   --config ./end-to-end.yml
