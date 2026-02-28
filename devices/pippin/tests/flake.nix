32aa27eedc6b404d1e62914a946fdc8abf2d4180
Scott Muc <scott@scottmuc.com>
2026-02-28 23:33:18 +0100

    Install CA certs in CI image for navidrome

--- devices/pippin/tests/flake.nix
+++ devices/pippin/tests/flake.nix
@@ -29,6 +29,7 @@
         name = "navidrome-ci";
         tag = "latest";
         contents = [
+          pkgs.dockerTools.caCertificates # installs CAs into expected /etc/ssl/certs
           pkgs.dockerTools.usrBinEnv # provides /usr/bin/env
           pkgs.dockerTools.fakeNss # provides /etc/passwd and /etc/group so that getpwuid() works
           (pkgs.buildEnv {
@@ -45,6 +46,7 @@
           Env = [
             "PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}"
             "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true"
+            "NODE_EXTRA_CA_CERTS=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
           ];
         };
       };
