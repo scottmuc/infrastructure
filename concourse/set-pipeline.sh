f14d16d496e633916c0d59d87f54b5e79eab5082
Scott Muc <scott@scottmuc.com>
2026-03-02 20:26:13 +0100

    Create a suite of registry-image put examples

--- concourse/set-pipeline.sh
+++ concourse/set-pipeline.sh
@@ -6,3 +6,9 @@ fly -t concourse.scottmuc.com \
   set-pipeline \
   --pipeline mucrastructure \
   --config ./mucrastructure.yml
+
+
+fly -t concourse.scottmuc.com \
+  set-pipeline \
+  --pipeline registry-image-testing \
+  --config ./registry-image-test.yml
