251323d6acb692ce2f755805063d542ca2fb77a3
Scott Muc <scott@scottmuc.com>
2026-03-23 22:28:42 +0100

    Enable drift detection

--- devices/ansible.sh
+++ devices/ansible.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+drift_detection_args=""
+
 playbook=$(find ./ -wholename "*/playbooks/*.yml" \
   | sort -r \
   | grep -v bootstrap \
   | gum choose --height 20 --header "Select a playbook")
 
-gum confirm "Running ${playbook}" || exit 1
+if gum confirm "Drift detection only?"; then
+  drift_detection_args="--check --diff"
+fi
 
 set -x
+# shellcheck disable=SC2086
 env \
   ANSIBLE_CONFIG=./ansible.cfg \
 ansible-playbook \
   --inventory ./inventory.yml \
-  "${playbook}"
+  "${playbook}" ${drift_detection_args}
