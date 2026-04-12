33d1a7ac09c4532f324b7c06b554724a76ca9c03
Scott Muc <scott@scottmuc.com>
2026-04-12 20:05:47 +0200

    Ditch todo-comments
    Adding a dependency to make comments pop feels a bit too much

--- homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/init.lua
+++ homedirs/common/dotfiles/dot.config/nvim/lua/lazy/plugins/init.lua
@@ -36,9 +36,6 @@ return {
     },
   },
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
