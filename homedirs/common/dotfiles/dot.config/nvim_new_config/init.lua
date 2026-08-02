d9687c4d33ab7a6061d982ff579949c8c15c53f4
Scott Muc <scott@scottmuc.com>
2026-08-02 11:38:57 +0200

    Bringing in more vim.opts to the new config

--- homedirs/common/dotfiles/dot.config/nvim_new_config/init.lua
+++ homedirs/common/dotfiles/dot.config/nvim_new_config/init.lua
@@ -1,27 +1,23 @@
--- I like my leader to be space... for reasons
+-- :use :help <option> to see what these things do
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- disable netrw from loading, needs to be one of the first things set
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-
--- enable 24-bit colour
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.opt.termguicolors = true
-
 vim.opt.number = true
 vim.opt.relativenumber = true
-
+vim.opt.scrolloff = 10
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.tabstop = 4
+vim.opt.breakindent = true
+vim.opt.undofile = true
 vim.opt.swapfile = false
-
--- always include a sign column which stops LSP warnings/errors from
--- reindenting things all the time
 vim.opt.signcolumn = 'yes'
-
--- enable the new loader, I believe this is required to use the built in
--- pluging dependency installer.
-vim.loader.enable()
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
 
 vim.pack.add({
 	{ src = 'https://github.com/neovim/nvim-lspconfig', },
